0c47063e01bd5f2e50b677e4c858853cc20508b0
goguma1206 <chiro9678@gmail.com>
2020-03-31 23:17:32 +0900

    백준 1일차

---
+++ Baekjoon/src/level1/LEVEL1.java
@@ -0,0 +1,13 @@
+package level1;
+
+public class LEVEL1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		//문제1. Hello World!를 출력하시오.
+				System.out.println("Hello World!");
+		
+	}
+	
+}
